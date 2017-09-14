6e23160b82c5ede3c7d6f8d76bbb1a63419c44d0
Yuuki Okamiya <yuuki.okamiya.yn@renesas.com>
2017-09-14 16:59:20 +0900

    First commit into GitHub.

---
+++ main.cpp
@@ -0,0 +1,33 @@
+// GR-PEACH mbed style project (template V2.02)
+// main.cpp: Public domain
+#include "mbed.h"
+
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
+
+int main() {
+    printf("blinky\r\n");
+
+    while(1) {
+        led1 = 1;
+        wait(0.2);
+        led2 = 1;
+        wait(0.2);
+        led3 = 1;
+        wait(0.2);
+        led4 = 1;
+        wait(0.2);
+        led1 = 0;
+        wait(0.2);
+        led2 = 0;
+        wait(0.2);
+        led3 = 0;
+        wait(0.2);
+        led4 = 0;
+        wait(0.2);
+    }
+}
+
+
